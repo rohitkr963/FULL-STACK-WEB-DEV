0b0cb3526f89b3327d9a3ea08b1cecb6fbbae377
rohitkr963 <nk7565331@gmail.com>
2024-11-21 23:18:08 +0530

    first commit

---
+++ JAVASCRIPT/FUNCTION IN JS/CalculateArea.js
@@ -0,0 +1,11 @@
+function area(a,b){
+    let area = a*b
+
+console.log(area)
+}
+area(5,6)
+
+/*      Output
+
+        30
+                    */
---
+++ JAVASCRIPT/FUNCTION IN JS/addTwoNumber.js
@@ -0,0 +1,14 @@
+function add(){
+    let a = 6
+    let b = 7
+    let add = a+b
+
+    console.log(add)
+}
+
+add()
+
+/*  Output
+
+       13 
+                */
