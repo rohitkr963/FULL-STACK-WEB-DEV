f1f96fe393573d9c5c49c73cbd20c98cea8e4594
rohitkr963 <nk7565331@gmail.com>
2024-11-21 23:25:59 +0530

    first commit

---
+++ JAVASCRIPT/FUNCTION IN JS/ASSIGNMENT/MarksTotalMarksAndPercentage.js
@@ -0,0 +1,21 @@
+function marks(){
+
+let math = 60
+let phy = 70
+let che = 55
+let eng = 76
+let hin = 81
+
+let total = math + phy + che + eng + hin
+
+let percentage = (total/500)*100.0
+
+console.log (`Total Marks = ${total}`)
+
+console.log(`percentage = ${percentage}%`)
+
+}
+
+marks()
+
+
