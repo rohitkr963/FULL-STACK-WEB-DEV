c6d9ee26a8923f8beb39bcaa5471f04b7dbba8db
rohitkr963 <nk7565331@gmail.com>
2024-11-21 23:24:53 +0530

    first commit

---
+++ JAVASCRIPT/FUNCTION IN JS/ASSIGNMENT/InitiakAmountAndExpenseAmount.js
@@ -0,0 +1,10 @@
+
+function Amaount(Initial){
+    let Expense = 20   // Copy Price
+
+    let Remaining = Initial - Expense
+
+    return Remaining
+}
+
+console.log(`The Remaining Balance Is = ${Amaount(100)}`)   // Initial Balance 100
