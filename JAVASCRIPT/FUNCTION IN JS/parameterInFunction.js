216c565fbf56ec9937b4e7b0eddd1b95d423ec9f
rohitkr963 <nk7565331@gmail.com>
2024-11-21 23:19:50 +0530

    first commit

---
+++ JAVASCRIPT/FUNCTION IN JS/parameterInFunction.js
@@ -0,0 +1,12 @@
+function argument(a,b){         //parameter
+    let sub = a-b;
+    console.log(sub)
+}
+
+argument(5,3)                   //Argument
+
+
+/*      Output
+
+        2
+                    */
