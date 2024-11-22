815f28091797a6e14a1d3c76e19007f7e3700dcc
rohitkr963 <nk7565331@gmail.com>
2024-11-22 22:28:44 +0530

    first commit

---
+++ JAVASCRIPT/MORE FUNCTION/ASSIGNMENT/PredictTheOutput.js
@@ -0,0 +1,26 @@
+let arrowFunction = (name = 'Coders') => {
+    console.log(`Welcome ${name}`);
+}
+console.log(arrowFunction('Programmers'))
+
+
+/*       Output
+    
+    Welcome Programmers
+    undefined
+
+*/
+
+/*  let arrowFunction = (name = 'Coders') => {
+    console.log(`Welcome ${name}`);
+    }
+    arrowFunction()
+
+*/
+
+/*       2nd Output
+    
+    Welcome Programmers
+    
+
+*/
