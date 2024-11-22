e3099f1b85a66150847c9bf1c0d1bd30a849b1e6
rohitkr963 <nk7565331@gmail.com>
2024-11-22 22:21:00 +0530

    first commit

---
+++ JAVASCRIPT/MORE FUNCTION/CovertCelciusToForenheight.js
@@ -0,0 +1,9 @@
+/*  const celciusToForen = (c) => {
+    return (c * 9 / 5) + 32;
+}
+
+console.log(celciusToForen(25))  */
+
+const celciusToForen = c => (c * 9 / 5) + 32;
+
+console.log(celciusToForen(25))
