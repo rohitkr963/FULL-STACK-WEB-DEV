6e68d1b324da59d5a7e43b8692527869042f2d36
rohitkr963 <nk7565331@gmail.com>
2024-11-22 22:26:28 +0530

    first commit

---
+++ JAVASCRIPT/MORE FUNCTION/SelfInvokingFunction.js
@@ -0,0 +1,4 @@
+(function dis () {          //SelfInvoking Function Without call automatic call
+    console.log("hello");
+})()
+                            //IIFF - Immediatly invoke Function expression
