976ca0167979325f462883c98053ea5c1db90319
rohitkr963 <nk7565331@gmail.com>
2024-11-22 22:27:52 +0530

    first commit

---
+++ JAVASCRIPT/MORE FUNCTION/ASSIGNMENT/ArrowFunctionPerfectSquar.js
@@ -0,0 +1,9 @@
+const Squar = (Height,Weidth) => {
+    if(Height == Weidth){
+        console.log("Squar is Perfect")
+    }
+    else{
+        console.log("Squar is Not Perfect")
+    }
+}
+Squar(4,4)
