05e5465d0eaac1d471e887ff71b8340775034b77
Rohit Kashyap <nk7565331@gmail.com>
2025-01-11 18:25:41 +0530

    commit

--- JAVASCRIPT/MORE FUNCTION/SelfInvokingFunction.js
+++ JAVASCRIPT/MORE FUNCTION/SelfInvokingFunction.js
@@ -1,4 +1,4 @@
 (function dis () {          //SelfInvoking Function Without call automatic call
-    console.log("hello")
+    console.log("hello");
 })()
                             //IIFF - Immediatly invoke Function expression
